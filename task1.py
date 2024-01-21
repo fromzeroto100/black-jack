269eb27a3b7ed814a92aebc8ffece6b230d887b0
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-21 19:56:55 +0100

    last

--- task1.py
+++ task1.py
@@ -6,16 +6,7 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    user_cards.append(deal_card)
-    computer_cards.append(deal_card)
-
 def calculate_score(cards):
-
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
@@ -27,3 +18,51 @@ def calculate_score(cards):
     # user_cards.sum()
     # computer_cards.sum()
 
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "Lose, you went over"
+    elif computer_score > 21:
+        return "Opponent went ower, You win!"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+while not is_game_over:
+
+
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    print(f"  Your cards: {user_cards}, current score: {user_score}")
+    print(f"  Computer's first card: {computer_cards[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")  
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True    
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+print(compare(user_score, computer_score))
