27af4f29ecacda52c3a76ffd1b531685cf68157e
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-21 15:03:50 +0100

    new1

--- task1.py
+++ task1.py
@@ -1,8 +1,18 @@
 import random
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-print("Do you want to play? yes or no")
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-if "yes":
-    
+
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card)
+    computer_cards.append(deal_card)
+
+   
+
